f15aded763a098f40e43d9f00f3910facfe71c97
Yoni Friedman <yoniifriedman@gmail.com>
2018-09-08 21:12:46 -0400

    added text-to-speech script

--- ttts.py
+++ ttts.py
@@ -1,68 +1,68 @@
 from google.cloud import texttospeech
-import extract
+import generate_transcript as gt
+from pydub import AudioSegment 
 
-source_audio = 'path/to/flac/file'
-script = 'path/to/script'
+#eventually make this more configurable
+def generate_characters():
 
-stt = gen_transcript(source_audio, script)
-# Instantiates a client
-client = texttospeech.TextToSpeechClient()
-#ben, leslie, andy
-characters = {"Ben": "en-US-Wavenet-A", "Leslie": "en-US-Wavenet-C", "Andy": "en-US-Wavenet-B"}
+        
+#        ben_voice = texttospeech.types.VoiceSelectionParams(name='de-DE-Wavenet-B',
+ #       language_code="en-US")
+  #      andy_voice = texttospeech.types.VoiceSelectionParams(name='de-DE-Wavenet-D',
+   #     language_code="en-US")
+    #    leslie_voice = texttospeech.types.VoiceSelectionParams(name='de-DE-Wavenet-A',
+     #   language_code="en-US")
 
+        characters = {"Ben": 'de-DE-Wavenet-B', "Leslie": 'de-DE-Wavenet-A', "Andy": 'de-DE-Wavenet-D'}
 
+        return characters
 
-def generate(characters):
-        # Set the text input to be synthesized
-        synthesis_input = texttospeech.types.SynthesisInput(text="Hello, World!")
-
-        # Build the voice request, select the language code ("en-US") and the ssml
-        # voice gender ("neutral")
-        #voice = texttospeech.types.VoiceSelectionParams(
-        #    language_code='en-US',
-        #    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
-
-        ben_voice = texttospeech.types.VoiceSelectionParams(name=characters["Ben"],
-        language_code="en-US")
-
-        andy_voice = texttospeech.types.VoiceSelectionParams(name=characters["Andy"],
-        language_code="en-US")
-
-        leslie_voice = texttospeech.types.VoiceSelectionParams(name=characters["Leslie"],
-        language_code="en-US")
-
-        client = texttospeech.TextToSpeechClient()
-        ben_input = texttospeech.types.SynthesisInput(text="Hello, I'm Ben")
-        leslie_input = texttospeech.types.SynthesisInput(text="Hello, I'm Leslie")
-        andy_input = texttospeech.types.SynthesisInput(text="Hello, I'm Andy")
-
+def generate_audio_clip(client, node, node_num, language_code, characters):
         audio_config = texttospeech.types.AudioConfig(
         audio_encoding=texttospeech.enums.AudioEncoding.MP3)
 
         # Perform the text-to-speech request on the text input with the selected
         # voice parameters and audio file type
+        snippet = configure_speaker_snippet(node, characters, client, language_code, audio_config)
+        
 
-        ben_response = client.synthesize_speech(ben_input, ben_voice, audio_config)
-        leslie_response = client.synthesize_speech(leslie_input, leslie_voice, audio_config)
-        andy_response = client.synthesize_speech(andy_input, andy_voice, audio_config)
-
-
+        output_file = "media/output"+str(node_num)+".mp3"
         # The response's audio_content is binary.
-        with open('output.mp3', 'wb') as out:
+        with open(output_file, 'wb') as out:
         # Write the response to the output file.
-            out.write(ben_response.audio_content)
-            print('Audio content written to file "ben_output.mp3"')
-        
-        with open('leslie_output.mp3', 'wb') as out:
-        # Write the response to the output file.
-            out.write(leslie_response.audio_content)
-            print('Audio content written to file "leslie_output.mp3"')
+            out.write(snippet.audio_content)
+            print('Audio content written to output file ' + str(node_num))
+        time_delta = node.end - node.start        
+        audio = AudioSegment.from_file(output_file)
+        audio.duration_seconds = time_delta
+        audio.export(output_file, format='mp3')
 
-        with open('andy_output.mp3', 'wb') as out:
-        # Write the response to the output file.
-            out.write(andy_response.audio_content)
-            print('Audio content written to file "andy_output.mp3"')
-        
+def configure_speaker_snippet(node, character_voices, client, language_code, audio_config):
+    voice = configure_voice(node, character_voices, language_code)    
+    synthesis_input = texttospeech.types.SynthesisInput(text=node.sentence)
+    snippet = client.synthesize_speech(synthesis_input, voice, audio_config)
+    return snippet
+
+def configure_voice(node, character_voices, language_code):
+    voice = texttospeech.types.VoiceSelectionParams(name=character_voices[node.speaker], language_code=language_code)
+    return voice
 
 
-generate(characters)
+source_audio = 'output.flac'
+#need the transcript here
+script = 'script.txt'
+
+#Get timed audio transcript
+transcript = gt.gen_transcript(source_audio, script, 'de')
+
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+node_num = 1
+
+characters = generate_characters()
+
+#eventually adapt this to take command line input for dynamic language change
+for node in transcript:
+    generate_audio_clip(client, node, node_num, 'de', characters)
+    node_num = node_num + 1
+return transcript
